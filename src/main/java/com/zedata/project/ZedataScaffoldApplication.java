c6d71126c8fc87a48c871acf01901b99f8cc7c71
Mr-Glacier <K15100415112@163.com>
2025-01-20 00:35:35 +0800

    init

---
+++ src/main/java/com/zedata/project/ZedataScaffoldApplication.java
@@ -0,0 +1,17 @@
+package com.zedata.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mr-Glacier
+ * @since  2025/1/20
+ */
+@SpringBootApplication
+public class ZedataScaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZedataScaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zedata/project/ZedataScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zedata.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZedataScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
